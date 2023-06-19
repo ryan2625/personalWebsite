69a3798cbc990673ad4d66e8955560692bb01753
ryan2625 <ryanfreas19@augustana.edu>
2023-06-19 00:04:58 -0500

    Adding coloring to images

--- src/conponenets/homeC/homeContent.jsx
+++ src/conponenets/homeC/homeContent.jsx
@@ -2,7 +2,7 @@ import "./homeContent.css"
 export const HomeContent = () =>{
     return <>
     <div className="home">
-        <h1 id="namer">Hi, I'am Ryan</h1>
+        <h1 id="namer"></h1>
     </div>
     <div className="firstCard">
         asd
