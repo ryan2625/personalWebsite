f8fdfbd4849cd9f40f8e2bfd6fdc8215a9cf6ad9
ryan2625 <ryanfreas19@augustana.edu>
2023-06-19 02:35:08 -0500

    Home page

--- src/conponenets/homeC/homeContent.jsx
+++ src/conponenets/homeC/homeContent.jsx
@@ -1,11 +1,15 @@
 import "./homeContent.css" 
 export const HomeContent = () =>{
     return <>
-    <div className="home">
-        <h1 id="namer"></h1>
-    </div>
-    <div className="firstCard">
-        asd
+    <div className="homeContainer2">
+        
+         <div className="namer left">
+            <h1 className="qLeft">Who am I?</h1></div>
+         <div className="bio right">
+            <h4 className="aRight">
+             I am a webdeveloper with expertise in technologies such as React, HTML and css.
+            </h4>
+            </div>
     </div>
     </>
 }
