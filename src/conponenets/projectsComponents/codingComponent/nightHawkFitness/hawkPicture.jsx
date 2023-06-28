e8655c024ef67b658fde28e5d99d2b240cb69453
ryan2625 <ryanfreas19@augustana.edu>
2023-06-28 02:14:31 -0500

    adding responsive about section for nighthawk

--- src/conponenets/projectsComponents/codingComponent/nightHawkFitness/hawkPicture.jsx
+++ src/conponenets/projectsComponents/codingComponent/nightHawkFitness/hawkPicture.jsx
@@ -3,7 +3,7 @@ import "./hawkPicture.css"
 import image from "./intro.gif"
 export const Nhawk = () => {
     return <div className="pictureHawk">
-        <img src={image} alt="" />
+        <img className="hawkGif" src={image} alt="" />
         <div className="topHawk"><h1>Dashboard</h1></div>
         <div className="bottomHawk"><h1>Workout Plan</h1></div>
     </div>
